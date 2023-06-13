48d578d97c06f7ea94ddadd706787d98a8f9ad05
umidnm <umidnm@code.edu.az>
2023-06-13 14:41:36 +0400

    Api craeted for first time

---
+++ FirstApiApp/FirstApiApp/DAL/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using FirstApiApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApiApp.DAL
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) { }
+        
+
+        public DbSet<Group> Groups { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ FirstApiApp/FirstApiApp/Models/Group.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstApiApp.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
